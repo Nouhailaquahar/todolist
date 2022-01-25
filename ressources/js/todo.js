1f6529b398f4accf7c345b7ce410cadde8a2cf7c
Nouhailaquahar <nouhaila.quahar.officiel.com>
2022-01-25 13:31:53 +0100

    first commit

--- ressources/js/todo.js
+++ ressources/js/todo.js
@@ -1,15 +1,50 @@
-window.onload=function(){
-  
- // var table=document.getElementsByClassName("modif");
-  var supp=document.getElementsByClassName("supp");
-/*for (i=0;i<table.length;i++) {
-      table[i].addEventListener("click",modification);}*/
-for (j=0;j<supp.length;j++) {
-      supp[j].addEventListener("click",suppression);}
+var idel = document.getElementsByClassName("fa-trash");
+for (var i = 0; i < idel.length; i++) {
+idel[i].addEventListener("click",deleteTask);
 }
-function suppression(){
-  var confirmation=confirm("vous etes sur?");
-  if (confirmation) {
-      this.parentElement.parentElement.remove();
+var vars = document.getElementsByClassName("fa-user-edit");
+for (var i = 0; i < vars.length; i++) {
+vars[i].addEventListener("click",editTaskico);
+}
+
+function deleteTask(){
+    var conf =confirm("Are u sure?")   
+      if(conf)
+        this.parentElement.parentElement.remove();
+}
+
+var list = document.getElementsByTagName("li");
+for (var i = 0; i < list.length; i++) {
+list[i].children[0].addEventListener("click",editTask);
+}
+
+function editTask(){
+    this.setAttribute("contenteditable","")
+
+}
+function editTaskico(){
+    this.parentElement.parentElement.children[0].setAttribbute("contenteditable","")
+
+}
+document.getElementById("btnadd").addEventListener("click",add)
+function add(){
+    var task=document.getElementById("ctn").textContent
+    document.getElementById("ulg").innerHTML+='<li class="list-group-item"> <span> '+task+'</span><div class="icons"><i class="fa fa-trash" title="delete" onclick="deleteTask"></i> <i class="fa fa-pencil " title="edit"></i</div></li>'
+    if(task!=""){
+      document.getElementById("ctn").textContent=""
+    }
+  
+else{
+    alert("Task not avalable");
+}
   }
+function recherche(){
+  var cle=document.getElementById("Search").value
+  
+  for(i=0;i<list.length;i++){
+    if(!list[i].textContent.toLowerCase().includes(cle.toLowerCase()))
+    list[i].style.display='none'
+    else{list[i].style.display='block'}
   }
+  
+}
